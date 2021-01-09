47331faaf5de0301b4658c2985b10235229161bb
ecorral <ecorral@intecca.uned.es>
2021-01-09 11:04:18 +0100

    Eventos columnas, resize window

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -8,8 +8,24 @@ import movies from 'src/assets/movies.json';
 })
 export class BodyComponent implements OnInit {
   moviesList: any = movies;
+  columnas: number = 2;
   constructor() {
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.columnas = (window.innerWidth <= 1400) ? 3 : 4;
+  }
+
+  onResize(e : any) {
+    if (e.target.innerWidth <= 1400 && e.target.innerWidth > 1100) {
+      this.columnas = 3;
+    } else if (e.target.innerWidth <= 1100 && e.target.innerWidth > 750) {
+      this.columnas = 2;
+    } else if (e.target.innerWidth <=750) {
+      this.columnas = 1;
+    } else {
+      this.columnas = 4;
+    }
+    console.log(e.target.innerWidth);
+  }
 }
